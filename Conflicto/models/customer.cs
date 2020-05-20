f02fd5ab3807a024edbd912e33df348cc8a5e118
Carlos <carlos.castilla009@gmail.com>
2020-05-19 22:24:42 -0500

    initial

---
+++ Conflicto/models/customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conflicto.models
+{
+    public class customer
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int lastName { get; set; }
+        public string payment { get; set; }
+        public int edad { get; set; }
+        public int age { get; set; }
+        public asd MyProperty { get; set; }
+    }
+}
